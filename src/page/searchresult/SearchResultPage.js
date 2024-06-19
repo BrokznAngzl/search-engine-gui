8b2820bcb4b7b32c9b63717a18039b9e378897cc
Wisarut Beng <bukobeer001@gmail.com>
2024-06-19 21:53:04 +0700

    edit data receiving and showing

--- src/page/searchresult/SearchResultPage.js
+++ src/page/searchresult/SearchResultPage.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import HeaderComp from "./component/HeaderComp";
 import axios from "axios";
 import {useLocation} from 'react-router-dom';
+import parse from 'html-react-parser';
 
 
 const SearchResultPage = () => {
@@ -25,8 +26,8 @@ const SearchResultPage = () => {
                 }
             });
 
-            setData(response.data.data); // นี้ขึ้นอยู่กับโครงสร้างของ response ที่คุณได้รับมาจาก API
-            console.log(response.data.data)
+            setData(response.data.data);
+            // console.log(response.data.data)
         } catch (error) {
             setError('There was an error fetching the search results.');
             console.error('Error fetching search results:', error);
@@ -42,31 +43,37 @@ const SearchResultPage = () => {
             <HeaderComp queryInput={qParam}/>
             <main className="container mx-auto p-4">
                 <div className="bg-white   pl-16 rounded-lg">
+
                     {/* Display search results */}
                     {error && <p className="text-red-500">{error}</p>}
                     <ul>
-                        {data.map((item, index) => {
-                            const domain = new URL(item.link)
-                            const modifiedLink = domain.toString().replace(domain.origin, '').replace(/\//g, ' > ');
+                        {data && data.map((item, index) => {
+                            // const itemKey = item[0]
+                            const itemData = item[1]
+                            const domain = new URL(itemData.link)
+                            const subLink = domain.toString().replace(domain.origin, '').replace(/\//g, ' > ');
+
                             return (
                                 <li key={index} className="py-4">
                                     <div className="max-w-md">
                                         <div className="cursor-pointer mx-auto" onClick={() => {
-                                            window.location.href = item.link
+                                            window.location.href = itemData.link
                                         }}>
                                             <div className="flex items-center mb-2">
-                                                <img src={item.icon} alt={item.title}
+                                                <img src={itemData.icon} alt={itemData.title}
                                                      className="w-6 h-6 rounded-full shadow-md"/>
                                                 <div className="mx-4">
                                                     <p className="text-xs">{domain.hostname}</p>
-                                                    <p className="text-xs">{domain.origin}{modifiedLink}
+                                                    <p className="text-xs">{domain.origin}{subLink}
                                                     </p>
                                                 </div>
                                             </div>
 
-                                            <a href={item.link} className="text-blue-600">{item.title}</a>
+                                            <a href={itemData.link} className="text-blue-600">{itemData.title}</a>
                                         </div>
-                                        <p className="text-gray-700 text-xs">{item.description}</p>
+                                        <p className="text-gray-700 text-xs">
+                                            {parse(itemData.description.replace(qParam, '<strong>' + qParam + '</strong>'))}
+                                        </p>
                                     </div>
                                 </li>
                             )
